dcec7f831ae29d567154593fa5c90d0f8caf49f6
nkmrdesu <52124803+nkmrdesu@users.noreply.github.com>
2021-02-04 13:56:00 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -9,15 +9,17 @@ import discord
 client = discord.Client()
 token = os.environ['DISCORD_BOT_TOKEN']
 targetChannel = os.environ['TEST_CHANNEL']
-channel = client.get_channel(targetChannel)
-print('channelの存在確認')
-print(channel)
-print(targetChannel)
+
 
 # 起動時の処理
 @client.event
 async def on_ready():
     #otameshi
+    await client.wait_until_ready()
+    channel = client.get_channel(targetChannel)
+    print('channelの存在確認')
+    print(channel)
+    print(targetChannel)
     await channel.send('やっほー！おはよー騎士くん！')
     #ループ処理
 #     loop.start()
